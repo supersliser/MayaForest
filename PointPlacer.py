5d412572d7625a0769f1a89f14e257ec492010f9
supersliser <43965667+supersliser@users.noreply.github.com>
2024-01-31 21:18:26 +0000

    dsffsd

--- PointPlacer.py
+++ PointPlacer.py
@@ -1,6 +1,46 @@
 import maya.cmds as cmds
 import random as r
 
+
 class PointPlacer:
     points = []
-    def placePoints(baseMesh):
+
+    def __init__(self):
+        self.points = []
+
+    def is_vertex_at_height_percentage(self, meshName, vertexIndex, percentage):
+        # Get bounding box dimensions
+        min_bound = cmds.exactWorldBoundingBox(meshName)[1]
+        max_bound = cmds.exactWorldBoundingBox(meshName)[4]
+
+        # Calculate total height and target height
+        top_y = max_bound
+        bottom_y = min_bound
+        total_height = top_y - bottom_y
+        target_height = top_y - (total_height * percentage)
+
+        # Get vertex position
+        vertex_pos = cmds.pointPosition(
+            meshName + ".vtx[" + str(vertexIndex) + "]", w=True
+        )
+        vertex_y = vertex_pos[1]
+
+        # Compare vertex and target heights
+        return vertex_y >= target_height
+
+    def generatePoints(self, baseMesh):
+        self.points = cmds.ls(baseMesh + ".vtx[*]", fl=1)
+
+
+    def removePoints(self, baseMesh, density):
+        tempPoints = self.points
+        self.points = []
+        for p in tempPoints:
+            if r.random() <= density:
+                if self.is_vertex_at_height_percentage(meshName=baseMesh, vertexIndex=tempPoints.index(p), percentage=0.2):
+                    self.points.append(cmds.pointPosition(p, w=1))
+
+    def placePoints(self, branch):
+        for p in self.points:
+            name = cmds.spaceLocator(p=p)
+            cmds.parent(name, branch)
--- Tree.py
+++ Tree.py
@@ -2,42 +2,66 @@ import maya.cmds as cmds
 import random as r
 import math as m
 
-def createBranch(i, dec, branch, num, height):
+def set_pivot_to_bottom(obj_name):
+    # Get the bounding box of the object
+    bbox = cmds.exactWorldBoundingBox(obj_name)
+    
+    # Calculate the height of the object
+    height = bbox[4] - bbox[1]
+    
+    # Calculate the new pivot point
+    new_pivot = [0.0, -height/2, 0.0]
+    
+    # Set the pivot point of the object
+    cmds.xform(obj_name, piv=new_pivot, r=True)
+    return new_pivot
+
+def createBranch(i, dec, branch, high, den):
+	Pointy = PointPlacer()
+	Pointy.generatePoints(branch)
+	Pointy.placePoints(branch)
+	num = 0
 	if r.random() < i:
-		newName = branch + "_Branch" + str(num)
-		print("Creating branch: " + newName)
-		cmds.polyCylinder(n=newName, sx=1, sy=ySub * i + 1, sz=1, radius=radius * (1 - i), height=height * (1 - i))
+		Pointy.removePoints(branch, den)
+		for point in Pointy.points:
+			newName = branch + "_Branch" + str(num)
+			print("Creating branch: " + newName)
+			cmds.polyCylinder(n=newName, sx=1, sy=ySub, sz=1, radius=radius - i, height=high * i)
+			cmds.parent(newName, branch)
+			pivot = set_pivot_to_bottom(newName)
+			cmds.xform(newName, translation=(point[0] - pivot[0], point[1] - pivot[1], point[2] - pivot[2]), ws=1)
+			# # cmds.scale(i, i, i)
+			# # cmds.rotate("45deg", 0, 0, r=1)
+			cmds.xform(newName, ro=(str(45 * r.random()) + "deg", str(180 * r.random()) + "deg", str(45 * r.random()) + "deg"))
 
-		cmds.select(newName)
-		cmds.rotate("45deg", 0, 0, r=1)
-		#+ r.random() * (ySub*2*10 - height)
-		x, y, z = cmds.pointPosition(branch + ".vtx[" + str(m.floor(height * 2 * 10 )) + "]", w=True)
-		cmds.move(x, y, z)
-		#cmds.move(i * height, y=1, r=1)
-		cmds.select(cl=1)
+			# for j in range(10, (ySub * 2 * 10), 10):
+			# 	cmds.polySelect(newName, el=j)
+			# 	cmds.polyMoveEdge(tx=r.random() * Ta * 2 - Ta, tz=r.random() * Ta * 2 - Ta, sz=r.random() * Sa + 0.8, sx=r.random() * Sa + 0.8)
 
-		cmds.parent(newName, branch, r=1, add=1)
+			# cmds.select(cl=1)
+			# cmds.polySmooth(n, dv=2, kb=1)
 
-		# for i in range(10, ySub * 2 * 10, 10):
-		# 	cmds.polySelect(newName, el=i)
-		# 	cmds.polyMoveEdge(tx=i * (r.random() * Ta * 2 - Ta), tz=i * (r.random() * Ta * 2 - Ta), sz=i * (r.random() * Sa + 0.8), sx=i * (r.random() * Sa + 0.8))
-
-		# cmds.select(cl=1)
-		# cmds.polySmooth(n, dv=2, kb=1)
-
-		createBranch(i - dec, dec, newName, 1, height)
-
-cmds.file( f=True, new=True )
+			createBranch(i - dec, dec, newName, high, den)
+			num+= 1
+	else:
+		for point in Pointy.points:
+			newName = Branch + "_Leaf" + str(num)
+			cmds.instance("Leaf1", n=newName)
+			cmds.xform(newName, translation=(point[0], point[1], point[2]), ws=1)
+			num += 1
+# cmds.file( f=True, new=True )
 
 r.seed(1)
 
 radius = 0.5
-height = 20
+height = 50
 ySub = 10
 n = "Trunk"
 Ta = 0.1
 Sa = 0.5
-minBranch = (ySub * 2 * 10) - 300
+Density = 0.1
+
+cmds.delete(n)
 
 cmds.polyCylinder(n=n, sx=1, sy=ySub, sz=1, radius=radius, height=height)
 
@@ -47,5 +71,5 @@ for i in range(10, ySub * 2 * 10, 10):
 
 cmds.select(cl=1)
 cmds.polySmooth(n, dv=2, kb=1)
-
-createBranch(0.5, 0.1, n, 1, 75)
+set_pivot_to_bottom(n)
+createBranch(0.8, 0.2, n, height, Density)
