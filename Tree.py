dc1bc0248efa022adb1eeda4c80f61f62025b3fb
supersliser <43965667+supersliser@users.noreply.github.com>
2024-02-01 21:19:33 +0000

    dsa

--- Tree.py
+++ Tree.py
@@ -1,7 +1,6 @@
 import maya.cmds as cmds
 import random as r
 import math as m
-import threading
 
 def set_pivot_to_bottom(obj_name):
     # Get the bounding box of the object
@@ -22,7 +21,7 @@ def createBranch(i, dec, branch, high, den):
 	# Pointy.placePoints(branch)
 	num = 0
 	if r.random() < i:
-		Pointy.generatePoints(branch, den)
+		Pointy.generatePointsAbove(branch, den, 0.2)
 		for point in Pointy.points:
 			newName = branch + "_Branch" + str(num)
 			print("Creating branch: " + newName)
@@ -34,26 +33,19 @@ def createBranch(i, dec, branch, high, den):
 			# # cmds.rotate("45deg", 0, 0, r=1)
 			cmds.xform(newName, ro=(str(90 * r.random()) + "deg", str(180 * r.random()) + "deg", str(90 * r.random()) + "deg"))
 
-			# for j in range(10, (ySub * 2 * 10), 10):
-			# 	cmds.polySelect(newName, el=j)
-			# 	cmds.polyMoveEdge(tx=r.random() * Ta * 2 - Ta, tz=r.random() * Ta * 2 - Ta, sz=r.random() * Sa + 0.8, sx=r.random() * Sa + 0.8)
 
-			# cmds.select(cl=1)
-			# cmds.polySmooth(n, dv=2, kb=1)
-			threat = threading.Thread(target=createBranch(i - dec, dec, newName, high, den))
-			threat.start()
+			createBranch(i - dec, dec, newName, high, den)
 			num+= 1
-	elif branch != "Trunk":
-		Pointy.generatePoints(branch, den)
-		for point in Pointy.points:
-			newName = branch + "_Leaf" + str(num)
-			print("Creating leaf: " + newName)
-			cmds.instance("Leaf1", n=newName)
-			cmds.parent(newName, branch)
-			cmds.xform(newName, translation=(point[0], point[1], point[2]), ws=1)
-			cmds.xform(newName, ro=(str(180 * r.random()) + "deg", str(180 * r.random()) + "deg", str(180 * r.random()) + "deg"))
-			num += 1
-# cmds.file( f=True, new=True )
+	# elif branch != "Trunk":
+	# 	Pointy.generatePoints(branch, den)
+	# 	for point in Pointy.points:
+	# 		newName = branch + "_Leaf" + str(num)
+	# 		print("Creating leaf: " + newName)
+	# 		cmds.instance("Leaf1", n=newName)
+	# 		cmds.parent(newName, branch)
+	# 		cmds.xform(newName, translation=(point[0], point[1], point[2]), ws=1)
+	# 		cmds.xform(newName, ro=(str(180 * r.random()) + "deg", str(180 * r.random()) + "deg", str(180 * r.random()) + "deg"))
+	# 		num += 1
 
 r.seed(1)
 
@@ -63,7 +55,7 @@ ySub = 10
 n = "Trunk"
 Ta = 0.1
 Sa = 0.5
-Density = 0.05
+Density = 0.02
 
 
 cmds.polyCylinder(n=n, sx=1, sy=ySub, sz=1, radius=radius, height=height)
@@ -75,4 +67,4 @@ for i in range(10, ySub * 2 * 10, 10):
 cmds.select(cl=1)
 cmds.polySmooth(n, dv=2, kb=1)
 set_pivot_to_bottom(n)
-createBranch(0.4, 0.1, n, height, Density)
+createBranch(0.4, 0.4, n, height, Density)
