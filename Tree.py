4ae9a361a8e8efedf3bcf64903521516d79e13fd
supersliser <thomaslower9@gmail.com>
2024-02-01 15:31:27 +0000

    dsa

--- Tree.py
+++ Tree.py
@@ -17,36 +17,49 @@ def set_pivot_to_bottom(obj_name):
     cmds.xform(obj_name, piv=new_pivot, r=True)
     return new_pivot
 
-def createBranch(i, dec, branch, high, den):
-	Pointy = PointPlacer()
-	num = 0
-	if rnums.pop() < i:
-		if branch == "Trunk":
-			Pointy.generatePointsAbove(branch, den, height)
-		else:
-			Pointy.generatePoints(branch, den)
-		for point in Pointy.points:
-			newName = branch + "_Branch" + str(num)
-			print("Creating branch: " + newName)
-			cmds.instance(branch, n=newName)
-			cmds.parent(newName, branch)
-			cmds.scale(i, i * 2, i)
-			cmds.xform(newName, translation=(point[0] - pivot[0], point[1] - pivot[1], point[2] - pivot[2]), ws=1)
-			cmds.xform(newName, ro=(str(90 * rnums.pop()) + "deg", str(180 * rnums.pop()) + "deg", str(90 * rnums.pop()) + "deg"))
-			createBranch(i - dec, dec, newName, high, den)
-			num+= 1
-	elif branch != "Trunk":
-		Pointy.generatePoints(branch, den)
-		for point in Pointy.points:
-			newName = branch + "_Leaf" + str(num)
-			print("Creating leaf: " + newName)
-			cmds.instance("Leaf1", n=newName)
-			cmds.parent(newName, branch)
-			cmds.xform(newName, translation=(point[0], point[1], point[2]), ws=1)
-			cmds.xform(newName, ro=(str(180 * rnums.pop()) + "deg", str(180 * rnums.pop()) + "deg", str(180 * rnums.pop()) + "deg"))
-			num += 1
-	Pointy = None
+def createBranchNonRecursive(i, dec, branch, high, den):
+    stack = []
+    Pointy = PointPlacer()
+    num = 0
 
+    while True:
+        if rnums.pop() < i:
+            if branch == "Trunk":
+                Pointy.generatePointsAbove(branch, den, height)
+            else:
+                Pointy.generatePoints(branch, den)
+
+            for point in Pointy.points:
+                newName = branch + "_Branch" + str(num)
+                print("Creating branch: " + newName)
+                cmds.instance(branch, n=newName)
+                cmds.parent(newName, branch)
+                cmds.scale(i, i * 2, i)
+                cmds.xform(newName, translation=(point[0] - pivot[0], point[1] - pivot[1], point[2] - pivot[2]), ws=1)
+                cmds.xform(newName, ro=(str(90 * rnums.pop()) + "deg", str(180 * rnums.pop()) + "deg", str(90 * rnums.pop()) + "deg"))
+                
+                # Push the branch onto the stack along with its parameters
+                stack.append((i - dec, dec, newName, high, den, num))
+                num += 1
+
+        elif branch != "Trunk":
+            Pointy.generatePoints(branch, den)
+
+            for point in Pointy.points:
+                newName = branch + "_Leaf" + str(num)
+                print("Creating leaf: " + newName)
+                cmds.instance("Leaf1", n=newName)
+                cmds.parent(newName, branch)
+                cmds.xform(newName, translation=(point[0], point[1], point[2]), ws=1)
+                cmds.xform(newName, ro=(str(180 * rnums.pop()) + "deg", str(180 * rnums.pop()) + "deg", str(180 * rnums.pop()) + "deg"))
+                num += 1
+
+        if not stack:
+            break
+
+        # Pop the parameters from the stack
+        i, dec, branch, high, den, num = stack.pop()
+    
 r.seed(1)
 
 radius = 0.5
