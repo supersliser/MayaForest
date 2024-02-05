2ccebb5ad29ccb6ed9721e38b62423cf310d6c3d
supersliser <thomaslower9@gmail.com>
2024-02-05 15:11:09 +0000

    sdfjnds

--- Tree.py
+++ Tree.py
@@ -16,6 +16,20 @@ def set_pivot_to_bottom(obj_name):
     cmds.xform(obj_name, piv=new_pivot, r=True)
     return new_pivot
 
+def distortBranch(name):
+	cmds.polySelect(name, el=10)
+	cmds.scale(0.2, 1, 0.2, ls=1)
+	cmds.select(cl=1)
+	cmds.polySelect(name, el=ySub * 2 * 10)
+	cmds.scale(0.2, 1, 0.2, ls=1)
+	cmds.select(cl=1)
+	for j in range(20, (ySub * 2 * 10) - 10, 20):
+		cmds.polySelect(name, el=j)
+		cmds.polyMoveEdge(tx=r.random() * Ta * 2 - Ta, tz=r.random() * Ta * 2 - Ta, sz=r.random() * Sa + 0.8, sx=r.random() * Sa + 0.8, ws=0)
+		cmds.select(cl=1)
+
+
+
 def createBranch(i, dec, branch, den):
 	Pointy = PointPlacer()
 	# Pointy.placePoints(branch)
@@ -27,20 +41,17 @@ def createBranch(i, dec, branch, den):
 			print("Creating branch: " + newName)
 			cmds.polyCylinder(n=newName, sx=1, sy=ySub, sz=1, radius=radius * i, height=height * i)
 			cmds.parent(newName, branch)
+			if deform:
+				distortBranch(newName)
 			pivot = set_pivot_to_bottom(newName)
 			createBranch(i - dec, dec, newName, den)
 			cmds.xform(newName, translation=(point[0] - pivot[0], point[1] - pivot[1], point[2] - pivot[2]), ws=1)
 			# # cmds.scale(i, i, i)
 			# # cmds.rotate("45deg", 0, 0, r=1)
-			cmds.xform(newName, ws=0, ro=(str(90 * r.random()) + "deg", str(180 * r.random()) + "deg", str(90 * r.random()) + "deg"))
-			if deform:
-				for i in range(10, ySub * 2 * 10, 10):
-					cmds.polySelect(newName, el=i)
-					cmds.polyMoveEdge(tx=r.random() * Ta * 2 - Ta, tz=r.random() * Ta * 2 - Ta, sz=r.random() * Sa + 0.8, sx=r.random() * Sa + 0.8)
-					cmds.select(cl=1)
+			cmds.xform(newName, ws=1, ro=(str(90 * r.random()) + "deg", str(180 * r.random()) + "deg", str(90 * r.random()) + "deg"))
 			num+= 1
 	elif branch != "Trunk" and leaves:
-		Pointy.generatePointsAbove(branch, 0.5 - den, 0.2)
+		Pointy.generatePointsAbove(branch, (1 - den)/2, 0.2)
 		for point in Pointy.points:
 			newName = branch + "_Leaf" + str(num)
 			print("Creating leaf: " + newName)
@@ -49,6 +60,9 @@ def createBranch(i, dec, branch, den):
 			cmds.xform(newName, translation=(point[0], point[1], point[2]), ws=1)
 			cmds.xform(newName, ro=(str(360 * r.random()) + "deg", str(360 * r.random()) + "deg", str(360 * r.random()) + "deg"))
 			num += 1
+
+
+
 r.seed(1)
 
 radius = 1
@@ -57,21 +71,23 @@ ySub = 10
 n = "Trunk"
 Ta = 0.1
 Sa = 0.5
-Density = 0.3
+Density = 0.2
 leaves = True
-deform = False
+deform = True
 
 try:
     cmds.delete(n)
 except:
 	pass
 cmds.polyCylinder(n=n, sx=1, sy=ySub, sz=1, radius=radius, height=height)
+
+
 createBranch(0.8, 0.2, n, Density)
 
-for i in range(10, ySub * 2 * 10, 10):
-	cmds.polySelect(n, el=i)
-	cmds.polyMoveEdge(tx=r.random() * Ta * 2 - Ta, tz=r.random() * Ta * 2 - Ta, sz=r.random() * Sa + 0.8, sx=r.random() * Sa + 0.8)
+distortBranch("Trunk")
 
 cmds.select(n)
 cmds.polySmooth(dv=2, kb=1)
 set_pivot_to_bottom(n)
+
+cmds.refresh(f=1)
