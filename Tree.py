eec9b22e74b3a9cb4ef9179865bb7919f64ffe94
supersliser <thomaslower9@gmail.com>
2024-02-08 12:00:28 +0000

    fgd

--- Tree.py
+++ Tree.py
@@ -10,7 +10,7 @@ def set_pivot_to_bottom(obj_name):
     height = bbox[4] - bbox[1]
     
     # Calculate the new pivot point
-    new_pivot = [0.0, -height/2, 0.0]
+    new_pivot = [0.0, (-height/2) + 1, 0.0]
     
     # Set the pivot point of the object
     cmds.xform(obj_name, piv=new_pivot, r=True)
@@ -28,10 +28,11 @@ def distortBranch(name):
 		cmds.polyMoveEdge(tx=r.random() * Ta * 2 - Ta, tz=r.random() * Ta * 2 - Ta, sz=r.random() * Sa + 0.8, sx=r.random() * Sa + 0.8, ws=0)
 		cmds.select(cl=1)
 
-def editRadius(name, factor):
+def editRadius(name, factor, i):
+	i * 5
 	for j in range(20, (ySub * 2 * 10) - 10, 20):
 		cmds.polySelect(name, el=j)
-		cmds.scale(factor * (1 - (j / (ySub * 2 * 10))),1,factor * (1 - (j / (ySub * 2 * 10))))
+		cmds.scale(i * (1 - (j / (ySub * 2 * 10))) + i, 1, i * (1 - (j / (ySub * 2 * 10))) + i)
 		cmds.select(cl=1)
 
 def createBranch(i, dec, branch, den):
@@ -43,11 +44,11 @@ def createBranch(i, dec, branch, den):
 		for point in Pointy.points:
 			newName = branch + "_Branch" + str(num)
 			print("Creating branch: " + newName)
-			cmds.polyCylinder(n=newName, sx=1, sy=ySub, sz=1, radius=radius * i, height=height * i)
+			cmds.polyCylinder(n=newName, sx=1, sy=ySub, sz=1, radius=radius, height=height * i)
 			cmds.parent(newName, branch)
 			if deform:
 				distortBranch(newName)
-				editRadius(newName, widthFactor)
+				editRadius(newName, widthFactor, i)
 			pivot = set_pivot_to_bottom(newName)
 			createBranch(i - dec, dec, newName, den)
 			cmds.xform(newName, translation=(point[0] - pivot[0], point[1] - pivot[1], point[2] - pivot[2]), ws=1)
@@ -107,9 +108,11 @@ except:
 	pass
 cmds.polyCylinder(n=n, sx=1, sy=ySub, sz=1, radius=radius, height=height)
 
-editRadius(n, 5)
 
 createBranch(0.8, 0.2, n, Density)
+
+editRadius(n, 5, 5)
+
 
 distortBranch(n)
 
@@ -117,6 +120,6 @@ cmds.select(n)
 cmds.polySmooth(dv=2, kb=1)
 set_pivot_to_bottom(n)
 
-cmds.playbackOptions(minTime="0sec", maxTime="100sec", l="continuous")
+cmds.playbackOptions(minTime="0sec", maxTime="100frames", l="continuous")
 
 cmds.refresh(f=1)
