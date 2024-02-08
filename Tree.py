272b6521d6f753c75c872494f4e05b1c340555a9
supersliser <thomaslower9@gmail.com>
2024-02-08 11:21:09 +0000

    fgh

--- Tree.py
+++ Tree.py
@@ -17,18 +17,22 @@ def set_pivot_to_bottom(obj_name):
     return new_pivot
 
 def distortBranch(name):
-	cmds.polySelect(name, el=10)
-	cmds.scale(0.2, 1, 0.2, ls=1)
-	cmds.select(cl=1)
-	cmds.polySelect(name, el=ySub * 2 * 10)
-	cmds.scale(0.2, 1, 0.2, ls=1)
-	cmds.select(cl=1)
+	# cmds.polySelect(name, el=10)
+	# # cmds.scale(0.2, 1, 0.2, ls=1)
+	# cmds.select(cl=1)
+	# cmds.polySelect(name, el=ySub * 2 * 10)
+	# cmds.scale(0.2, 1, 0.2, ls=1)
+	# cmds.select(cl=1)
 	for j in range(20, (ySub * 2 * 10) - 10, 20):
 		cmds.polySelect(name, el=j)
 		cmds.polyMoveEdge(tx=r.random() * Ta * 2 - Ta, tz=r.random() * Ta * 2 - Ta, sz=r.random() * Sa + 0.8, sx=r.random() * Sa + 0.8, ws=0)
 		cmds.select(cl=1)
 
-
+def editRadius(name, factor):
+	for j in range(20, (ySub * 2 * 10) - 10, 20):
+		cmds.polySelect(name, el=j)
+		cmds.scale(factor * (1 - (j / (ySub * 2 * 10))),1,factor * (1 - (j / (ySub * 2 * 10))))
+		cmds.select(cl=1)
 
 def createBranch(i, dec, branch, den):
 	Pointy = PointPlacer()
@@ -43,6 +47,7 @@ def createBranch(i, dec, branch, den):
 			cmds.parent(newName, branch)
 			if deform:
 				distortBranch(newName)
+				editRadius(newName, widthFactor)
 			pivot = set_pivot_to_bottom(newName)
 			createBranch(i - dec, dec, newName, den)
 			cmds.xform(newName, translation=(point[0] - pivot[0], point[1] - pivot[1], point[2] - pivot[2]), ws=1)
@@ -93,7 +98,8 @@ Sa = 0.5
 Density = 0.2
 leaves = True
 deform = True
-animAmount = 10
+animAmount = 5
+widthFactor = 2
 
 try:
     cmds.delete(n)
@@ -101,10 +107,11 @@ except:
 	pass
 cmds.polyCylinder(n=n, sx=1, sy=ySub, sz=1, radius=radius, height=height)
 
+editRadius(n, 5)
 
 createBranch(0.8, 0.2, n, Density)
 
-distortBranch("Trunk")
+distortBranch(n)
 
 cmds.select(n)
 cmds.polySmooth(dv=2, kb=1)
