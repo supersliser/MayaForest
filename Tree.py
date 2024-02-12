c6fea03aa5dfa2b35f7cd86959ec0aa5c8190563
supersliser <thomaslower9@gmail.com>
2024-02-12 16:22:28 +0000

    fds

--- Tree.py
+++ Tree.py
@@ -35,39 +35,51 @@ def editRadius(name, factor, i):
 		cmds.scale(i * (1 - (j / (ySub * 2 * 10))) + i, 1, i * (1 - (j / (ySub * 2 * 10))) + i)
 		cmds.select(cl=1)
 
+
+def generateCurve(name: str, start: tuple, i: int = 1, rotation = (90, 0, 0)):
+    points:list = [start]
+    for j in range(m.floor(20 * i) + 1):
+        points.append((start[0] + (r.uniform(0, i/2)) * j, start[1] + j * i, start[2] + (r.uniform(0, i/2)) * j,))
+    cmds.curve(n=name, p=points, bez=1)
+    
+def generatePoints(name, density: float, height, i):
+    output = []
+    for j in range(r.randint(m.floor(density * 100) * 0.5, m.floor(density * 100) * 1.5)):
+        output.append(cmds.pointOnCurve(name, p=1, pr=r.uniform(height * ((i * 20) - 1), (i * 20) - 1)))
+    return output
+
+def sweepCurve(name, point, radius):
+    cmds.circle(n=name+"_profile", r=radius)
+    cmds.xform(t=point)
+    cmds.parent(name+"_profile", name)
+    cmds.xform(name+"_profile", ro=(90,0,0))
+    cmds.extrude(name+"_profile", name, et=2, n=name+"_mesh", fpt=1, p=point)
+    cmds.parent(name+"_mesh", name)
+    
+    # cmds.delete(name+"_profile")
+
 def createBranch(i, dec, branch, den):
-    Pointy = PointPlacer()
-    # Pointy.placePoints(branch)
     num = 0
     if r.random() < i:
-        Pointy.generatePointsAbove(branch, den, 0.5)
-        for point in Pointy.points:
+        points = generatePoints(branch, den, genHeight, i + dec)
+        for point in points:
             newName = f"{branch}_Branch{str(num)}"
             print(f"Creating branch: {newName}")
-            cmds.polyCylinder(n=newName, sx=1, sy=ySub, sz=1, radius=radius, height=height * i)
+            generateCurve(newName, point, i)
             cmds.parent(newName, branch)
-            if deform:
-                distortBranch(newName)
-                editRadius(newName, widthFactor, i)
-            pivot = set_pivot_to_bottom(newName)
-            createBranch(i - dec, dec, newName, den)
-            cmds.xform(newName, translation=(point[0] - pivot[0], point[1] - pivot[1], point[2] - pivot[2]), ws=1)
+            # pivot = set_pivot_to_bottom(newName)
+            # cmds.xform(newName, translation=(point[0] - pivot[0], point[1] - pivot[1], point[2] - pivot[2]), ws=1)
             # # cmds.scale(i, i, i)
             # # cmds.rotate("45deg", 0, 0, r=1)
-            cmds.xform(
-                newName,
-                ws=1,
-                ro=(
-                    f"{str(90 * r.random())}deg",
-                    f"{str(360 * r.random())}deg",
-                    f"{str(90 * r.random())}deg",
-                ),
-            )
-            createAnim(newName, cmds.xform(newName, q=1, ro=1), (1 - i) * animAmount, animationStart, animationStop, animationStep)
+            rotation = (f"{str(90 * r.random())}deg",f"{str(360 * r.random())}deg",f"{str(90 * r.random())}deg")
+            cmds.xform(newName,ws=1,rp=point, ro=rotation)
+            # createAnim(newName, cmds.xform(newName, q=1, ro=1), (1 - i) * animAmount, animationStart, animationStop, animationStep)
+            sweepCurve(newName, point, radius * i)
+            createBranch(i - dec, dec, newName, den)
             num+= 1
     elif branch != "Trunk" and leaves:
-        Pointy.generatePointsAbove(branch, (1 - (den * 10)) / 2, 0.5)
-        for point in Pointy.points:
+        points = generatePoints(branch, den, genHeight, i + dec)
+        for point in points:
             newName = f"{branch}_Leaf{str(num)}"
             print(f"Creating leaf: {newName}")
             cmds.duplicate("Leaf1", n=newName)
@@ -95,41 +107,44 @@ def createAnim(itemName, itemRotation, rotationFactor, start, stop, step):
 	
 	
 
-r.seed(2)
+r.seed(1)
 
-radius = 1
+radius = 0.5
 height = 40
 ySub = 10
 n = "Trunk"
 Ta = 0.1
 Sa = 0.5
-Density = 0.5
+Density = 0.8
 leaves = True
-deform = True
+deform = False
 animAmount = 10
 widthFactor = 2
 animationStart = 0
 animationStop = 200
 animationStep = 50
+genHeight = 1 - 0.3
 
 try:
     cmds.delete(n)
 except:
 	pass
-cmds.polyCylinder(n=n, sx=1, sy=ySub, sz=1, radius=radius, height=height)
 
+generateCurve(n, (0, 0, 0), 1)
+# cmds.polyCylinder(n=n, sx=1, sy=ySub, sz=1, radius=radius, height=height)
+sweepCurve(n, (0,0,0), radius)
 
 createBranch(0.8, 0.2, n, Density / 10)
 
-editRadius(n, 1, 2)
+# editRadius(n, 1, 2)
 
 
-distortBranch(n)
+# distortBranch(n)
 
-cmds.select(n)
-cmds.polySmooth(dv=2, kb=1)
-set_pivot_to_bottom(n)
+# cmds.select(n)
+# cmds.polySmooth(dv=2, kb=1)
+# set_pivot_to_bottom(n)
 
-cmds.playbackOptions(minTime=animationStart, maxTime=animationStop, l="continuous")
+# cmds.playbackOptions(minTime=animationStart, maxTime=animationStop, l="continuous")
 
 cmds.refresh(f=1)
