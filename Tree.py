1d0a1e40c8bcee36a844e806bb3210cec3947d46
supersliser <43965667+supersliser@users.noreply.github.com>
2024-02-14 12:39:24 +0000

    fds

--- Tree.py
+++ Tree.py
@@ -5,7 +5,7 @@ import math as m
 def generateCurve(name: str, height, start: tuple, i: int = 1):
     points:list = [start]
     for j in range(m.floor(height * i) + 1):
-        points.append((start[0] + (r.uniform(0, i/8)) * j, start[1] + j * i * 2, start[2] + (r.uniform(0, i/8)) * j,))
+        points.append((start[0] + (r.uniform(0, 0.2)) * j, start[1] + j * i * 2, start[2] + (r.uniform(0, 0.2)) * j,))
     cmds.curve(n=name, p=points, bez=1)
     
 def generatePoints(name, density: float, height, i):
@@ -14,15 +14,16 @@ def generatePoints(name, density: float, height, i):
         output.append(cmds.pointOnCurve(name, p=1, top=1, pr=r.uniform(height, 1)))
     return output
 
-def sweepCurve(name, point, radius):
+def sweepCurve(name, point, radius, i):
     cmds.circle(n=name+"_profile", r=radius)
     cmds.xform(t=point)
     cmds.parent(name+"_profile", name)
     cmds.xform(name+"_profile", ro=(90,0,0))
-    cmds.extrude(name+"_profile", name, et=2, n=name+"_mesh", fpt=1, p=point, sc=0.6)
+    cmds.extrude(name+"_profile", name, et=2, n=name+"_mesh", fpt=1, p=point, sc=i, rb=1, po=1)
     cmds.parent(name+"_mesh", name)
-    
     cmds.delete(name+"_profile")
+    cmds.polyNormal(name+"_mesh", nm=0)
+    cmds.hyperShade(name+"_mesh", a="BarkMat")
 
 def createBranch(i, dec, branch, den):
     num = 0
@@ -33,10 +34,10 @@ def createBranch(i, dec, branch, den):
             print(f"Creating branch: {newName}")
             generateCurve(newName, height, point, i)
             cmds.parent(newName, branch)
-            rotation = (f"{str(r.uniform(0, 90))}deg",f"{str(r.uniform(0, 180) * num)}deg",f"{str(r.uniform(0, 90))}deg")
+            rotation = (f"{str(r.uniform(20, 60))}deg",f"{str(r.uniform(0, 90) * num)}deg",f"{str(r.uniform(20, 60))}deg")
             cmds.xform(newName,ws=1,rp=point, ro=rotation)
             createAnim(newName, cmds.xform(newName, q=1, ro=1), (1 - i) * animAmount, animationStart, animationStop, animationStep)
-            sweepCurve(newName, point, radius * i)
+            sweepCurve(newName, point, radius * i, i)
             createBranch(i - dec, dec, newName, den)
             num+= 1
         elif branch != "Trunk" and leaves:
@@ -72,14 +73,14 @@ r.seed(5)
 radius = 1
 height = 20
 n = "Trunk"
-Density = 0.3
+Density = 0.2
 leaves = True
 deform = False
 animAmount = 10
 animationStart = 0
 animationStop = 200
 animationStep = 25
-genHeight = 1 - 0.5
+genHeight = 1 - 0.6
 branchDivStart = 0.8
 branchDec = 3
 
@@ -90,7 +91,7 @@ except:
 
 generateCurve(n,height, (0, 0, 0), 1)
 
-sweepCurve(n, (0,0,0), radius)
+sweepCurve(n, (0,0,0), radius * 2, 1)
 
 createBranch(branchDivStart, branchDivStart / branchDec, n, Density / 2)
 
